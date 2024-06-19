f5426cc703b02aeaccde13fee7d3d56f5292b644
Nipun Chamika <nipunchamika11@gmail.com>
2024-06-19 15:59:24 +0530

    feat: Implement create user and list users methods

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html', {'users': users})
+
+
+def user_create(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User created successfully!')
+            return redirect('user_list')
+
+    return render(request, 'user_create.html')
--- userhub/settings.py
+++ userhub/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +84,7 @@ DATABASES = {
         'NAME': 'users',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'N1pun$',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
     }
 }
 
